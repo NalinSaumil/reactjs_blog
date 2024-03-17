4e949e2e5227a2189730007277ce6f52d04eab53
NalinSaumil <nalinsaumil@gmail.com>
2024-03-17 17:35:46 -0500

    commit to lesson6: this is sick. So I learned about how to change information on the webApp. Basically, a hook is something that allows you to make a variable accessible from the webapp. So if you assign the value using a hook, you can also define a set method for the value and make the value reactive. Then you can invoke the setMethod from an action, say click event, and the hook will set the new value and access it from the website to change the websites value.

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,25 @@
+import { useState } from 'react';
+
 const Home = () => {
+    //let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25); 
 
-    const handleClick = (e) => {
-        console.log('Hello World', e);
-    };
-
-    const handleClickAgain = (name, e) => {
-        console.log('hello, ' + name, e.target);
+    const handleClick = () => {
+        if(name === 'mario'){
+            setName('luigi');
+            setAge(30);
+        } else {
+            setName('mario');
+            setAge(25);
+        }
     };
 
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old.</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click Me Again</button>
         </div>
     );
 }
