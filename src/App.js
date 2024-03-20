0cb1f27d17b8d2ba0f697b20fce40dab355e955c
NalinSaumil <nalinsaumil@gmail.com>
2024-03-19 23:16:14 -0500

    commit to lesson12: learned about the use effect hook, function that runs on every reload. Caution: learn how to useState within the useEffect function or else ENDLESS LOOOOOPPPPPP

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import Navbar from './Navbar'
 import Home from './Home'
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = true;
-  const link = "http://www.google.com";
-
-
+  
   return (
     <div className="App">
       <Navbar />
