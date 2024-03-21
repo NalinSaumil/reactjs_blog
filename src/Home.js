1766a417df703376f1453d695410737379172c47
NalinSaumil <nalinsaumil@gmail.com>
2024-03-21 12:55:20 -0500

    commit to lesson17: worked on handling error with .catch and throw functions. Also learned how to create your own errors. and that fetch will still return a res on an invalid endpoint so you need to use res.ok to check that res's validity.

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,33 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [err, setErr] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch data for that resource');
+                }
                 return res.json();
             })
             .then((data) => {
                 setBlogs(data);
                 setIsPending(false);
+                setErr(null);
+            })
+            .catch(err => {
+                setErr(err.message);
+                setIsPending(false);
+                setBlogs(null)
             });
         }, 1000);        
     }, []);
 
     return (  
         <div className="home">
+            {err && <div>{ err }</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={ blogs } title="All Blogs!" />}
         </div>
